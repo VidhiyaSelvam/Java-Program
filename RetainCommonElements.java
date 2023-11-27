4ea36ff689b9bf967c42e4365d02fe5646036005
VidhiyaSelvam <142381984+VidhiyaSelvam@users.noreply.github.com>
2023-11-27 21:54:04 +0530

    Add files via upload

---
+++ RetainCommonElements.java
@@ -0,0 +1,27 @@
+package com.vertical.solution.example;
+
+import java.util.HashSet;
+
+public class RetainCommonElements {
+		public static void main(String[] args) {
+			HashSet hs = new HashSet();
+			hs.add(1);
+			hs.add("two");
+			hs.add(3);
+			hs.add("four");
+			hs.add(5);
+			hs.add("Six");
+			hs.add(7);
+			hs.add("eight");
+			hs.add(9);
+			HashSet hsTwo = new HashSet();
+			hsTwo.add(1);
+			hsTwo.add("two");
+			hsTwo.add(3);
+			hsTwo.add("four");
+			hsTwo.add(5);
+			hs.retainAll(hsTwo);
+			System.out.println(hs);
+		}
+	}
+
