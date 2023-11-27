af9f18957604b137ec214972f4c8be062f79dbdf
VidhiyaSelvam <142381984+VidhiyaSelvam@users.noreply.github.com>
2023-11-27 21:53:52 +0530

    Add files via upload

---
+++ RemoveSymbols.java
@@ -0,0 +1,25 @@
+package com.vertical.solution.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RemoveSymbols {
+		public static void main(String[]args) {
+			Map<Integer,String> m = new  HashMap<Integer,String>();
+			m.put(1, "Thermo)meter");
+			m.put(2, "Fluctu%ation");
+			m.put(3, "Eagl$e");
+			m.put(4, "Pinp#oint");
+			for(Map.Entry<Integer,String> mp:m.entrySet()) {
+				Integer key =  mp.getKey();
+				String value = mp.getValue();
+				String modifiedValue = value.replaceAll("[!@#$%^&*()0]", "");
+				m.put(key, modifiedValue);
+			}
+			System.out.println(m);
+		}
+	}
+		
+	
+
+
