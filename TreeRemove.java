a0ec4ec25cb3538543b34b6cfe68c70cecd6b105
VidhiyaSelvam <142381984+VidhiyaSelvam@users.noreply.github.com>
2023-11-27 21:54:54 +0530

    Add files via upload

---
+++ TreeRemove.java
@@ -0,0 +1,20 @@
+package com.vertical.solution.example;
+
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class TreeRemove {	
+		public static void main(String[] args) {
+			TreeSet ts = new TreeSet();
+			ts.add(1);
+			ts.add(5);
+			ts.add(7);
+			ts.add(9);
+			Scanner s = new Scanner(System.in);
+			System.out.println("Enter a index to remove the element");
+			int index = s.nextInt();
+			ts.remove(index);
+			System.out.println(ts);
+		}
+	}
+
