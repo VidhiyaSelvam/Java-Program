ee6054d8f3d9c77cc40f2029032f9c7675a10790
VidhiyaSelvam <142381984+VidhiyaSelvam@users.noreply.github.com>
2023-11-27 21:52:48 +0530

    Add files via upload

---
+++ LinkedListAddRemove.java
@@ -0,0 +1,22 @@
+package com.vertical.solution.example;
+
+import java.util.LinkedList;
+
+public class LinkedListAddRemove {
+		public static void main(String[]args) {
+			LinkedList li = new LinkedList();
+			li.add("Naveen");
+			li.add("Nijantha");
+			li.add("Thilip");
+			li.add("Ganesh");
+			li.add("Samy");
+			li.addFirst("Roomies:");
+			li.addLast("!!!");
+			System.out.println(li);
+			li.removeFirst();
+			li.removeLast();
+			System.out.println(li);
+		}
+	}
+
+
