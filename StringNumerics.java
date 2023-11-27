f04429c163d58e949917be810190c8e290dceca1
VidhiyaSelvam <142381984+VidhiyaSelvam@users.noreply.github.com>
2023-11-27 21:54:16 +0530

    Add files via upload

---
+++ StringNumerics.java
@@ -0,0 +1,13 @@
+package com.vertical.solution.example;
+
+public class StringNumerics {
+	
+		public static void main(String[]args) {
+			String no = "1847254.412132141";
+			double d = Double.parseDouble(no);
+			int num = (int) d;
+			System.out.println(num);
+		}
+	}
+
+
