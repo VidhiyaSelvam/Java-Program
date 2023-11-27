12dea3888bcba3f04e3a6b39989f0ace909d19d6
VidhiyaSelvam <142381984+VidhiyaSelvam@users.noreply.github.com>
2023-11-27 21:55:08 +0530

    Add files via upload

---
+++ WideningNarrowing.java
@@ -0,0 +1,18 @@
+package com.vertical.solution.example;
+
+public class WideningNarrowing {
+		public static void main(String[]args) {
+			//implicit-Widening
+			int no = 10;
+			double num = no;
+			System.out.println(no);
+			System.out.println(num);
+			//Explicit-Narrowing
+			double d = 33.33;
+			int dd = (int)d;
+			System.out.println(d);
+			System.out.println(dd);
+		}
+	}
+
+
